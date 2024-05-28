6adcdb7e78ad3e9d7679678344931416f2fb74e4
Othallia <vianiputriocta6@gmail.com>
2024-05-28 19:21:00 +0700

    menghapus koma

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -1,22 +1,4 @@
-// solveMultiwaris.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or #include <iostream>
 using namespace std;
 
 class orang {
@@ -30,7 +12,7 @@ public:
     }
 };
 
-class pekerja :virtual public orang {
+class pekerja : public orang {
 public:
     pekerja(int pUmur) :
         orang(pUmur)
@@ -53,8 +35,8 @@ class budi : public pekerja, public pelajar {
 public:
     budi(int pUmur) :
         pekerja(pUmur),
-        pelajar(pUmur),
-        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+        pelajar(pUmur)
+       
     {
         cout << "Budi dibuat\n" << endl;
     }
@@ -64,5 +46,4 @@ int main() {
     budi a(12);
 
     return 0;
-} > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+} 
